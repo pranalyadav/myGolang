ad053965b402bce6c8a3bd8f9cc3c3b61724a765
pranalYadav <yadav.pranal94@gmail.com>
2026-02-04 10:46:59 +0530

    update a course conntroller in golang

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -96,3 +96,27 @@ func createOneCourse(w http.ResponseWriter, r *http.Request ) {
 	json.NewEncoder(w).Encode(course)
 	return
 }
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//first grab id from request
+	params := mux.Vars(r)
+
+	//loop, id, remove, add with my id
+
+	for index, course := range courses {
+		if course.CourseId == params["id"]{
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	//TODO: send a response when id is not found
+
+}
