fdf25d2e1c76c3d4ec68a530f04d6727d06628c5
pranalYadav <yadav.pranal94@gmail.com>
2026-02-23 12:11:36 +0530

    waitgroups in golang

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
+	// "time"
 )
 
+var wg sync.WaitGroup //pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://github.com",
+		"https://facebook.com",
+	}
+
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
 }
 
-func greeter(s string) {
+// func greeter(s string) {
 
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-	fmt.Println(s)
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 	fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+	fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 	}
 }
